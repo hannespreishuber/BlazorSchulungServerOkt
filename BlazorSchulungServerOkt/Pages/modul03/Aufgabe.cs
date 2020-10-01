b2131d066d981fd5775db9da0426e67332ea154d
Hannes Preishuber <hannesp@ppedv.de>
2020-10-01 16:00:19 +0200

    Todo Model

---
+++ BlazorSchulungServerOkt/Pages/modul03/Aufgabe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSchulungServerOkt.Pages.modul03
+{
+    public class Aufgabe
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public DateTime ErledigtBis { get; set; }
+        public bool Erledigt { get; set; }
+        [NotMapped]
+        public bool IsEdit { get; set; }
+    }
+}
