1ff08d94e428fb9633a10a5e68bd010e43467b47
Hannes Preishuber <hannesp@ppedv.de>
2020-10-01 11:18:04 +0200

    Blazor Component und Codebehind

---
+++ BlazorSchulungServerOkt/Pages/modul01/Component1.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSchulungServerOkt.Pages.modul01
+{
+    public partial class Component1
+    {
+
+
+        protected override void OnInitialized()
+        {
+            File.ReadAllLines(@"c:\temp\test.txt");
+        }
+    }
+}
