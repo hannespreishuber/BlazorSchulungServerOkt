d41cd3cca41e7cea61f10314cf4da9e554df5b68
Hannes Preishuber <hannesp@ppedv.de>
2020-10-02 09:39:36 +0200

    Live Ticker (ala Chat) ViewModel mit Event und UI Update

---
+++ BlazorSchulungServerOkt/Pages/modul03/LiveTicker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSchulungServerOkt.Pages.modul03
+{
+    public class LiveTicker
+    {
+        public List<TickerEintrag> TickerListe { get; set; }
+        public event Action OnPropertyChanged;
+
+        public LiveTicker()
+        {
+            TickerListe = new List<TickerEintrag>();
+        }
+        public TickerEintrag Add(string Text)
+        {
+            var t = new TickerEintrag();
+            t.ID = TickerListe.Count() + 1;
+            t.Text = Text;
+            t.Zeit = DateTime.Now;
+            TickerListe.Add(t);
+            OnPropertyChanged?.Invoke();
+            return t;
+
+        }
+    }
+    public class TickerEintrag
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public DateTime Zeit { get; set; }
+
+    }
+}
--- BlazorSchulungServerOkt/Startup.cs
+++ BlazorSchulungServerOkt/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorSchulungServerOkt.Data;
+using BlazorSchulungServerOkt.Pages.modul03;
 
 namespace BlazorSchulungServerOkt
 {
@@ -29,6 +30,7 @@ namespace BlazorSchulungServerOkt
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<LiveTicker>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
