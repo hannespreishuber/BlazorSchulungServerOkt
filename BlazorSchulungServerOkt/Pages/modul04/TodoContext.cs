bd49806173404d52d8d4269a7b27cef6b402cc22
Hannes Preishuber <hannesp@ppedv.de>
2020-10-02 11:08:16 +0200

    Datenbank Entity Framework

---
+++ BlazorSchulungServerOkt/Pages/modul04/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using BlazorSchulungServerOkt.Pages.modul03;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSchulungServerOkt.Pages.modul04
+{
+    public class TodoContext:DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext>options):base(options)
+        {
+
+        }
+        public virtual DbSet<Aufgabe> Aufgabes { get; set; }
+    }
+}
