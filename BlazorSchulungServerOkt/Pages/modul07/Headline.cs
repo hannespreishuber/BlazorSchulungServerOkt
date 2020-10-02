ee06dc83bc9468be827398574072773516979e91
Hannes Preishuber <hannesp@ppedv.de>
2020-10-02 15:43:56 +0200

    Chart Sample SVG

---
+++ BlazorSchulungServerOkt/Pages/modul07/Headline.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSchulungServerOkt.Pages.modul07
+{
+    public class Headline : ComponentBase
+    {
+        [Parameter]
+        public string MyProperty { get; set; }
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(1, "H1");
+            builder.AddContent(2, MyProperty);
+            builder.CloseElement();
+        }
+    }
+}
